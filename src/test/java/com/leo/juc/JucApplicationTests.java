e40bef30adb1c26020a9e76200c91b27ee34f93b
hal <202816368+HAL9386@users.noreply.github.com>
2026-03-08 11:43:21 +0800

    initialize Spring Boot application with Maven wrapper and basic project structure

---
+++ src/test/java/com/leo/juc/JucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
